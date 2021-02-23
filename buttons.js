2b97ba52e2cfb67929c0d7c10a936d97f64f200e
LaytonAvery <laytonavery5@gmail.com>
2021-02-23 12:25:26 -0500

    initial commit

---
+++ buttons.js
@@ -0,0 +1,10 @@
+let pinkButton = document.getElementById("pinkButton");
+let blueButton = document.getElementById("blueButton");
+
+pinkButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "pink";
+});
+
+blueButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
